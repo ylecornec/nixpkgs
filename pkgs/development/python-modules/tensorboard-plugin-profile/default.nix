b4899486b8048a20b42263d01e69e42fb8f7e53b
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-08-26 21:32:45 +0200

    python3Packages.tensorboard_plugin_profile: 2.4.0 -> 2.5.0

--- pkgs/development/python-modules/tensorboard-plugin-profile/default.nix
+++ pkgs/development/python-modules/tensorboard-plugin-profile/default.nix
@@ -6,14 +6,14 @@
 
 buildPythonPackage rec {
   pname = "tensorboard_plugin_profile";
-  version = "2.4.0";
+  version = "2.5.0";
   format = "wheel";
 
   src = fetchPypi {
     inherit pname version;
     format = "wheel";
     python = "py3";
-    sha256 = "0z6dcjvkk3pzmmmjxi2ybawnfshz5qa3ga92kqj69ld1g9k3i9bj";
+    sha256 = "f832698d87a773b9a017fc4dd5cf598a1ff942ccfbf3392c83fe12c949ab9f52";
   };
 
   propagatedBuildInputs = [
