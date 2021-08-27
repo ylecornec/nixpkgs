373f8a89245b0d82c98bb0bfc7c25fb64c6bc08d
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-08-27 02:25:52 +0200

    python3Packages: provide setuptools-scm with toml[i] by default and remove duplicated toml packages
    Some packages seem to still require toml despite setuptools-scm having switched to tomli.
    If it is missing the version number in dist.into is set to 0.0.0 and silently all version pins break.

--- pkgs/development/python-modules/setuptools-scm/default.nix
+++ pkgs/development/python-modules/setuptools-scm/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildPythonPackage, fetchPypi, toml }:
+{ lib, buildPythonPackage, fetchPypi, toml, tomli }:
 
 buildPythonPackage rec {
   pname = "setuptools-scm";
@@ -10,7 +10,9 @@ buildPythonPackage rec {
     sha256 = "0e9fc7ca3f15657941d352ee2be5901d1606294577213e0f4c26cef20978e819";
   };
 
-  propagatedBuildInputs = [ toml ];
+  # TODO: figure out why both toml and tomli are needed when only tomli is listed in setuptools-scm
+  # if not both are listed some packages like zipp silently fallback to a 0.0.0 version number and break version pins in other packages
+  propagatedBuildInputs = [ toml tomli ];
 
   # Requires pytest, circular dependency
   doCheck = false;
