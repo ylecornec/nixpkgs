bd6833180b3cecdb5a61874d2c9f3f9576b3d0a2
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-08-27 02:34:32 +0200

    python39Packages.tomli: break infinite recursion with setuptools-scm by splitting tests, add SuperSandro2000 as maintainer

---
+++ pkgs/development/python-modules/tomli/tests.nix
@@ -0,0 +1,20 @@
+{ buildPythonPackage
+, tomli
+, pytestCheckHook
+, python-dateutil
+}:
+
+buildPythonPackage rec {
+  pname = "tomli-tests";
+  inherit (tomli) version;
+
+  src = tomli.testsout;
+
+  dontBuild = true;
+  dontInstall = true;
+
+  checkInputs = [
+    pytestCheckHook
+    python-dateutil
+  ];
+}
